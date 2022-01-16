87fba2407d37c1e7cc24da72c9c1c7fa2986546c
Javier Guerra <javier.guerra@konghq.com>
2022-01-15 20:07:33 -0500

    add golangcli-lint to install script

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -2,3 +2,5 @@
 
 go install ./cmd/protoc-gen-go-wrpc
 go install google.golang.org/protobuf/cmd/protoc-gen-go
+
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0
